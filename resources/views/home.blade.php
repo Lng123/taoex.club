0796422c7481dfc241ede3afd8a003c08f6ad2dd
John Lee <johnlee46@gmail.com>
2019-11-17 19:39:57 -0800

    Updated on messaging tags
    messaging tags

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -116,7 +116,7 @@
                       <table class="table table-striped table-bordered">
                         <thead>
                           <tr data-toggle="collapse" data-target=".contents">
-                            <th>Sender</th>
+                            <th>Receiver</th>
                             <th>Messages</th>
                             <th>Time</th>
                           </tr>
